b04808ef6ac7469cbdf9b7a4e86de4209fd69bb4
Aymane <aymane.sennoussi@gmail.com>
2023-10-06 17:37:06 +0400

    debugger admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import FilteredEmails, Jobs
+from .models import FilteredEmails, Jobs, EmailDebugInfo
 
 # Register your models here.
 admin.site.register(FilteredEmails)
 admin.site.register(Jobs)
+admin.site.register(EmailDebugInfo)
